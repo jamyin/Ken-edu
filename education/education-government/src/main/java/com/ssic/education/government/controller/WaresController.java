76c610af3a508d7852ad8cac14bcb0d7b12607fb
mark <mark_chu@ssic.cn>
2016-05-12 17:06:49 +0800

    原料 或者 成品菜 处理 模块

---
+++ education/education-government/src/main/java/com/ssic/education/government/controller/WaresController.java
@@ -0,0 +1,19 @@
+package com.ssic.education.government.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+	 * 此类描述的是：原料 或者 成品菜 处理 模块
+	 * @author: cwftalus@163.com
+	 * @version: 2016年5月12日 下午5:05:51
+ */
+
+@Controller
+@RequestMapping(value="wares")
+public class WaresController extends BaseController{
+
+	
+
+}
