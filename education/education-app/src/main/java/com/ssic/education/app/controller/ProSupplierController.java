b2c447bf0744e6b37d6a2dc8273ebf735cff6afb
Tnomos <tnomos@gmail.com>
2016-05-12 11:33:05 +0800

    创建供应商相关类

---
+++ education/education-app/src/main/java/com/ssic/education/app/controller/ProSupplierController.java
@@ -0,0 +1,17 @@
+package com.ssic.education.app.controller;
+
+/**		
+ * <p>Title: ProSupplierController </p>
+ * <p>Description: 类描述</p>
+ * <p>Copyright (c) 2016 </p>
+ * <p>Company: 上海天坊信息科技有限公司</p>
+ * @author SeanYoung
+ * @date 2016年5月12日 上午10:57:58	
+ * @version 1.0
+ * <p>修改人：SeanYoung</p>
+ * <p>修改时间：2016年5月12日 上午10:57:58</p>
+ * <p>修改备注：</p>
+ */
+public class ProSupplierController {
+
+}
---
+++ education/education-app/src/main/java/com/ssic/education/app/service/impl/ProSupplierServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ssic.education.app.service.impl;
+
+/**		
+ * <p>Title: ProSupplierServiceImpl </p>
+ * <p>Description: 类描述</p>
+ * <p>Copyright (c) 2016 </p>
+ * <p>Company: 上海天坊信息科技有限公司</p>
+ * @author SeanYoung
+ * @date 2016年5月12日 上午10:57:34	
+ * @version 1.0
+ * <p>修改人：SeanYoung</p>
+ * <p>修改时间：2016年5月12日 上午10:57:34</p>
+ * <p>修改备注：</p>
+ */
+public class ProSupplierServiceImpl {
+
+}
+
