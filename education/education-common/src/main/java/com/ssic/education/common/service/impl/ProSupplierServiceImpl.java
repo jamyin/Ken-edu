3cda06e17524a6a314dfbab4443d72fadd1b12f7
pengpeng_zhang <pengpeng_zhang@ssic.cn>
2016-05-12 09:05:54 +0800

    方法的实现

--- education/education-common/src/main/java/com/ssic/education/common/service/impl/ProSupplierServiceImpl.java
+++ education/education-common/src/main/java/com/ssic/education/common/service/impl/ProSupplierServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssic.education.common.dao.ProSupplierDao;
+import com.ssic.education.government.dto.ProSupplierDto;
+import com.ssic.education.utils.util.BeanUtils;
 
 @Service
 public class ProSupplierServiceImpl {
@@ -11,8 +13,8 @@ public class ProSupplierServiceImpl {
 	@Autowired
 	private ProSupplierDao proSupplierDao;
 	
-	public ProSuplierDto findById(String id) {
-		
+	public ProSupplierDto findById(String id) {
+		return BeanUtils.createBeanByTarget(proSupplierDao.selectByPrimaryKey(id), ProSupplierDto.class);
 	}
 	
 }
