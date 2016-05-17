c74a2f62027aba2a8fbe9f6e0e247d890a5cbd46
SeanYoung <gang_yang@ssic.cn>
2016-05-17 16:08:00 +0800

    菜谱管理 人员管理 系统管理控制器类

---
+++ education/education-provider/src/main/java/com/ssic/education/provider/controller/PersonController.java
@@ -0,0 +1,22 @@
+package com.ssic.education.provider.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**		
+ * <p>Title: PersonController </p>
+ * <p>Description: 类描述</p>
+ * <p>Copyright (c) 2016 </p>
+ * <p>Company: 上海天坊信息科技有限公司</p>
+ * @author SeanYoung
+ * @date 2016年5月17日 下午3:56:42	
+ * @version 1.0
+ * <p>修改人：SeanYoung</p>
+ * <p>修改时间：2016年5月17日 下午3:56:42</p>
+ * <p>修改备注：</p>
+ */
+@Controller
+@RequestMapping("/person")
+public class PersonController {
+
+}
