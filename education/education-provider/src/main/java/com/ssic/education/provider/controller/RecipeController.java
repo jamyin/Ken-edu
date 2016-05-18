36fd7fb307b37fdad3a8bd4b06b07bd4d93856ca
SeanYoung <gang_yang@ssic.cn>
2016-05-18 09:21:15 +0800

    文件修改

--- education/education-provider/src/main/java/com/ssic/education/provider/controller/RecipeController.java
+++ education/education-provider/src/main/java/com/ssic/education/provider/controller/RecipeController.java
@@ -1,11 +1,15 @@
 package com.ssic.education.provider.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ssic.util.model.Response;
+
 /**		
  * <p>Title: PackageController </p>
- * <p>Description: 类描述</p>
+ * <p>Description: 菜谱管理控制器类</p>
  * <p>Copyright (c) 2016 </p>
  * <p>Company: 上海天坊信息科技有限公司</p>
  * @author SeanYoung
@@ -18,5 +22,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/recipe")
 public class RecipeController {
+
+	/**
+	 * list列表页面
+	 * manager：一句话描述方法功能
+	 * @param request
+	 * @return
+	 * @exception	
+	 * @author SeanYoung
+	 * @date 2016年5月17日 下午5:22:27
+	 */
+	@RequestMapping("/list")
+	public String manager(HttpServletRequest request) {
+		return "recipe/list";
+	}
+
+	public Response<?> getPackegesList() {
+		return null;
+	}
 
 }
