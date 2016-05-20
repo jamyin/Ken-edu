0da2da9947ca231aaf04279d797b2c1799128fbd
SeanYoung <gang_yang@ssic.cn>
2016-05-20 14:41:34 +0800

    用户相关接口类

---
+++ education/education-app/src/main/java/com/ssic/education/app/mapper/AppUsersMapper.java
@@ -0,0 +1,13 @@
+package com.ssic.education.app.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.ssic.education.app.dto.EduAppUserDto;
+import com.ssic.education.app.dto.EduUsersInfoDto;
+
+public interface AppUsersMapper {
+
+	//List<TImsUsersDto> findBy(@Param("user") TImsUsersDto user);
+
+	EduAppUserDto selectLoginInfo(@Param("user") EduUsersInfoDto user);
+}
---
+++ education/education-app/src/main/java/com/ssic/education/app/service/impl/EduAppUsersServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ssic.education.app.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssic.education.app.dao.EduAppUsersDao;
+import com.ssic.education.app.dto.EduAppUserDto;
+import com.ssic.education.app.dto.EduUsersInfoDto;
+import com.ssic.education.app.service.IEduAppUsersService;
+import com.ssic.education.app.util.MD5Util;
+
+@Service
+public class EduAppUsersServiceImpl implements IEduAppUsersService {
+
+	@Autowired
+	private EduAppUsersDao eduUsersDao;
+
+	@Override
+	public EduAppUserDto appLogin(EduUsersInfoDto user) {
+		user.setPassword(MD5Util.md5(user.getPassword()));
+		EduAppUserDto result = eduUsersDao.appLogin(user);
+		return result;
+	}
+
+}
