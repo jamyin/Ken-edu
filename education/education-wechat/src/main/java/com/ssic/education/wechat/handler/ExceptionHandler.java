1e3c28478b0013d74cc843096445762e01b29e9d
mark <mark_chu@ssic.cn>
2016-05-20 09:59:40 +0800

    wechat 应用 启动

---
+++ education/education-wechat/src/main/java/com/ssic/education/wechat/handler/ExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.ssic.education.wechat.handler;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ssic.education.wechat.exception.EduException;
+import com.ssic.util.JsonUtil;
+import com.ssic.util.LogWriter;
+import com.ssic.util.constants.DataStatus;
+import com.ssic.util.model.Response;
+
+/**
+ * 统一异常处理器
+ * @author rkzhang
+ */
+public class ExceptionHandler implements HandlerExceptionResolver  {
+
+    private static final Logger log = Logger.getLogger(ExceptionHandler.class);
+	
+	public ModelAndView resolveException(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex) {
+   
+	        log.error("exception message :"+ex.getMessage());
+	        log.error(LogWriter.getStackMsg(ex.getStackTrace()));
+	        
+	        if(ex instanceof EduException) {
+	            EduException appEx = (EduException) ex;
+    	        	try {
+        	        	response.setContentType("application/json");
+        	        	response.setCharacterEncoding("utf-8");
+        			response.getWriter().print(JsonUtil.getJsonStr(new Response<String>(appEx.getErrorCode() == null ? DataStatus.HTTP_FAILE : appEx.getErrorCode(), appEx.getMessage(), null)));
+        			response.getWriter().close();
+        		} catch (IOException e) {
+        			e.printStackTrace();
+        		};
+	        }
+	        return new ModelAndView();  
+	}
+
+
+}
