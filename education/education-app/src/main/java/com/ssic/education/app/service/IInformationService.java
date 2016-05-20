211fcd19c82e1b6273e5df500d3f53187089b2ab
jamyin <jamhihi@126.com>
2016-05-20 11:47:42 +0800

    Signed-off-by: jamyin <jamhihi@126.com>

---
+++ education/education-app/src/main/java/com/ssic/education/app/service/IInformationService.java
@@ -0,0 +1,24 @@
+package com.ssic.education.app.service;
+
+import com.ssic.education.common.dto.EduInformationDto;
+import com.ssic.util.model.PageQuery;
+import com.ssic.util.model.PageResult;
+
+/**
+ * @ClassName: IInformationService
+ * @Description: App资讯接口(1:公告 2:卫生检查 3:健康宣教)  
+ * @author Ken Yin
+ * @date 2016年5月20日 上午10:42:48
+ *
+ */
+public interface IInformationService {
+
+	//查询所有资讯列表-带分页
+	PageResult<EduInformationDto> findInformationList(EduInformationDto eduInformationDto,
+			PageQuery query);
+	//根据学校id查资讯信息
+	EduInformationDto findInformationDetial(String id);
+
+
+}
+
---
+++ education/education-app/src/main/java/com/ssic/education/app/service/impl/InformationServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ssic.education.app.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssic.education.app.service.IInformationService;
+import com.ssic.education.common.dao.InformationDao;
+import com.ssic.education.common.dto.EduInformationDto;
+import com.ssic.education.common.pojo.EduInformation;
+import com.ssic.util.BeanUtils;
+import com.ssic.util.model.PageQuery;
+import com.ssic.util.model.PageResult;
+
+/**
+ * @ClassName: InformationServiceImpl
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author Ken Yin
+ * @date 2016年5月20日 上午11:02:07
+ *
+ */
+@Service
+public class InformationServiceImpl implements IInformationService{
+
+	@Autowired
+	private InformationDao informationDao;
+
+	@Override
+	public PageResult<EduInformationDto> findInformationList(EduInformationDto eduInformationDto,
+			PageQuery query) {
+		List<EduInformation> list = informationDao.findInformationList(eduInformationDto, query);
+		List<EduInformationDto> InformationDtoList = BeanUtils.createBeanListByTarget(list, EduInformationDto.class);
+		int total = informationDao.selectInformationAccount(eduInformationDto);
+		query.setTotal(total);
+		return new PageResult<EduInformationDto>(query, InformationDtoList);
+	}
+
+	@Override
+	public EduInformationDto findInformationDetial(String id) {
+		EduInformation eduInformation = informationDao.findInformationDetial(id);
+		EduInformationDto dtoList = BeanUtils.createBeanByTarget(eduInformation, EduInformationDto.class);
+		return dtoList;
+	}
+
+
+}
+
