52add433aac46cb1019b1941ebbeb9cc776b0d86
jamyin <jamhihi@126.com>
2016-05-21 12:25:21 +0800

    Signed-off-by: jamyin <jamhihi@126.com>

---
+++ education/education-app/src/main/java/com/ssic/education/app/service/impl/TaskServiceImpl.java
@@ -0,0 +1,61 @@
+package com.ssic.education.app.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssic.education.app.service.ITaskService;
+import com.ssic.education.common.dao.TaskDao;
+import com.ssic.education.common.dto.EduTaskDto;
+import com.ssic.education.common.pojo.EduTask;
+import com.ssic.util.BeanUtils;
+import com.ssic.util.model.PageQuery;
+import com.ssic.util.model.PageResult;
+
+/**	
+* @ClassName: SchoolServiceImpl
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author Ken Yin
+* @date 2016年5月12日 下午2:20:58
+*
+ */
+@Service
+public class TaskServiceImpl implements ITaskService{
+
+    @Autowired
+    private TaskDao taskDao;
+
+	@Override
+	public PageResult<EduTaskDto> findTaskListById(String id, int readstat,
+			PageQuery query) {
+		List<EduTaskDto> list = taskDao.findTaskListById(id, readstat, query);
+		int total = taskDao.selectTaskAccount(id, readstat);
+		query.setTotal(total);
+		return new PageResult<EduTaskDto>(query, list);
+	}
+
+	@Override
+	public PageResult<EduTaskDto> findSendListById(String id, PageQuery query) {
+		List<EduTask> list = taskDao.findSendListById(id, query);
+		List<EduTaskDto> dtoList = BeanUtils.createBeanListByTarget(list, EduTaskDto.class);
+		int total = taskDao.selectSendAccount(id);
+		query.setTotal(total);
+		return new PageResult<EduTaskDto>(query, dtoList);
+	}
+	
+	@Override
+	public Integer delTask(String id) {
+		EduTask task = new EduTask();
+		task.setId(id);
+		return taskDao.updateByPrimaryKeySelective(task);
+	}
+
+	@Override
+	public Integer updateTask(String id, String receiveId) {
+		return taskDao.updateTask(id, receiveId);
+	}
+
+	
+}
+
---
+++ education/education-common/src/main/java/com/ssic/education/common/mapper/EduTaskExMapper.java
@@ -0,0 +1,17 @@
+package com.ssic.education.common.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.ssic.education.common.dto.EduTaskDto;
+import com.ssic.util.model.PageQuery;
+
+
+public interface EduTaskExMapper {
+
+	List<EduTaskDto> findTaskListById(@Param("id") String id,@Param("readstat") int readstat,@Param("query")  PageQuery query);
+
+	int selectTaskAccount(@Param("id") String id,@Param("readstat") int readstat);
+
+}
