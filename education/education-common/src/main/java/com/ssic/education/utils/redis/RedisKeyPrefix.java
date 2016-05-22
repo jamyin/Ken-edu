51ad0f263cfb27c65b14f93b5d8c4991cefdd35f
mark <mark_chu@ssic.cn>
2016-05-22 15:42:03 +0800

    app 项目 运行正常

--- education/education-common/src/main/java/com/ssic/education/utils/redis/RedisKeyPrefix.java
+++ education/education-common/src/main/java/com/ssic/education/utils/redis/RedisKeyPrefix.java
@@ -1,15 +1,13 @@
 package com.ssic.education.utils.redis;
 
-import java.lang.annotation.ElementType;
+import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface RedisKeyPrefix {
-
-	String prefixValue();
-
-	
+@Target({java.lang.annotation.ElementType.TYPE})
+public @interface RedisKeyPrefix
+{
+  public abstract String prefixValue();
 }
--- education/education-common/src/main/java/com/ssic/education/utils/redis/RedisKeyResolver.java
+++ education/education-common/src/main/java/com/ssic/education/utils/redis/RedisKeyResolver.java
@@ -1,88 +1,87 @@
 package com.ssic.education.utils.redis;
 
-import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.ssic.education.utils.exception.SystemException;
 import com.ssic.education.utils.util.StringUtils;
 import com.ssic.education.utils.util.reflect.DynamicMethod;
 import com.ssic.education.utils.util.reflect.ReflectStringUtil;
 
-@Component
-public class RedisKeyResolver<T> {
+public class RedisKeyResolver<T>
+{
+  private static final Pattern pattern = Pattern.compile("\\{.*?\\}");
 
-	private static final String SPLIT_CHAR = ":";
-	
-	/**
-	 * 获取RedisKeyPrefix annotation
-	 * @param cls
-	 * @return
-	 */
-	public RedisKeyPrefix getRedisKeyPrefix(Class<? extends Object> cls) {
-		RedisKeyPrefix prefix = (RedisKeyPrefix) cls.getAnnotation(RedisKeyPrefix.class);
-		return prefix;
-	}
-	
-	/**
-	 * 根据对象类型和值获取key值
-	 * @param cls
-	 * @param value
-	 * @return
-	 */
-	public String getRedisKey(Class<? extends Object> cls, String value) {
-		RedisKeyPrefix keyPrefix = this.getRedisKeyPrefix(cls);
-		return this.paseKey(keyPrefix, value);
-	}
-	
-	//------获取单对象存储键值
-	public String paseKey(RedisKeyPrefix prefix, String value){
-		if(StringUtils.isEmpty(prefix.prefixValue())){
-			throw new SystemException("prefixValue can not be null");
-		}
-		return populateKey(prefix, value); 
-	}
-	
-	public String paseKey(RedisKeyPrefix prefix, T t) {
-		if(StringUtils.isEmpty(prefix.prefixValue())){
-			throw new SystemException("prefixValue can not be null");
-		}
-		String[] keyParams = StringUtils.split(prefix.prefixValue(), SPLIT_CHAR);
-		if(keyParams.length != 3){
-			throw new SystemException("prefixValue 格式错误");
-		}
-		String keyFiled = keyParams[2];
-		String keyFiledValue = DynamicMethod.invokeMethod(t, ReflectStringUtil.populateGetMethodName(keyFiled)).toString();
-		return populateKey(prefix, keyFiledValue);
-	}
-	
+  public RedisKeyPrefix getRedisKeyPrefix(Class<? extends Object> cls)
+  {
+    RedisKeyPrefix prefix = (RedisKeyPrefix)cls.getAnnotation(RedisKeyPrefix.class);
 
-	private String populateKey(RedisKeyPrefix prefix, String value) {
-		return prefix.prefixValue() + SPLIT_CHAR + value;
-	}
-	
-	//------获取List存储的键值
-	
-	public String paseKeyForList(RedisKeyPrefix prefix, String value){
-		if(StringUtils.isEmpty(prefix.prefixValue())){
-			throw new SystemException("prefixValue can not be null");
-		}
-		return populateKeyForList(prefix, value); 
-	}
-	
-	public String paseKeyForList(RedisKeyPrefix prefix, T t) {
-		if(StringUtils.isEmpty(prefix.prefixValue())){
-			throw new SystemException("prefixValue can not be null");
-		}
-		String[] keyParams = StringUtils.split(prefix.prefixValue(), SPLIT_CHAR);
-		if(keyParams.length != 3){
-			throw new SystemException("prefixValue 格式错误");
-		}
-		String keyFiled = keyParams[2];
-		String keyFiledValue = DynamicMethod.invokeMethod(t, ReflectStringUtil.populateGetMethodName(keyFiled)).toString();
-		return populateKeyForList(prefix, keyFiledValue);
-	}
-	
-	private String populateKeyForList(RedisKeyPrefix prefix, String keyFiledValue) {
-		return prefix.prefixValue() + SPLIT_CHAR + keyFiledValue + SPLIT_CHAR + "forlist";
-	}
+    return prefix;
+  }
 
+  public String getRedisKey(Class<? extends Object> cls, String value)
+  {
+    RedisKeyPrefix keyPrefix = getRedisKeyPrefix(cls);
+    return paseKey(keyPrefix, value);
+  }
+
+  public String paseKey(RedisKeyPrefix prefix, String value)
+  {
+    if (StringUtils.isEmpty(prefix.prefixValue())) {
+      throw new SystemException("prefixValue can not be null");
+    }
+    return populateKey(prefix, value);
+  }
+
+  public String paseKey(RedisKeyPrefix prefix, T t) {
+    String keyStr = prefix.prefixValue();
+    if (StringUtils.isEmpty(keyStr)) {
+      throw new SystemException("prefixValue can not be null");
+    }
+    Matcher m = pattern.matcher(keyStr);
+    while (m.find()) {
+      String keyFiled = m.group();
+      String filedName = keyFiled.replace("{", "");
+      filedName = filedName.replace("}", "");
+      String keyFiledValue = DynamicMethod.invokeMethod(t, ReflectStringUtil.populateGetMethodName(filedName)).toString();
+      keyFiledValue = StringUtils.isEmpty(keyFiledValue) ? "" : keyFiledValue;
+      keyStr = keyStr.replace(keyFiled, keyFiledValue);
+    }
+
+    return keyStr;
+  }
+
+  private String populateKey(RedisKeyPrefix prefix, String value) {
+    String keyStr = prefix.prefixValue();
+    if (StringUtils.isEmpty(keyStr)) {
+      throw new SystemException("prefixValue can not be null");
+    }
+    Matcher m = pattern.matcher(keyStr);
+    while (m.find()) {
+      String keyFiled = m.group();
+      value = StringUtils.isEmpty(value) ? "" : value;
+      keyStr = keyStr.replace(keyFiled, value);
+    }
+    return keyStr;
+  }
+
+  public String paseKeyForList(RedisKeyPrefix prefix, String value)
+  {
+    if (StringUtils.isEmpty(prefix.prefixValue())) {
+      throw new SystemException("prefixValue can not be null");
+    }
+    return populateKeyForList(prefix, value);
+  }
+
+  private String populateKeyForList(RedisKeyPrefix prefix, String value)
+  {
+    String keyStr = populateKey(prefix, value);
+    return keyStr + ":list";
+  }
+
+  public String paseKeyForList(RedisKeyPrefix prefix, T t) {
+    String keyStr = paseKey(prefix, t);
+    return keyStr + ":list";
+  }
 }
