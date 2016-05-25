0d3b4153760ace1e565fe6495a6070826e313eef
pengpeng_zhang <pengpeng_zhang@ssic.cn>
2016-05-25 17:17:30 +0800

    enum

---
+++ education/education-utils/src/main/java/com/ssic/education/utils/constants/ProNutritionalNameEnum.java
@@ -0,0 +1,48 @@
+package com.ssic.education.utils.constants;
+
+/**
+ * @ClassName: SupplyPhaseEnum
+ * @Description: 套餐供应阶段枚举类
+ * @author Ken Yin
+ * @date 2016年5月25日 上午9:52:53
+ *
+ */
+public enum ProNutritionalNameEnum {
+	ENERGY("能量", "能量"),VITAMIN("维生素", "维生素"),PROTEIN("蛋白质", "蛋白质");
+	private String index;
+	private String value;
+	ProNutritionalNameEnum(String index, String value){
+		this.index = index;
+		this.value = value;
+	}
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	/**
+	 * 根据枚举index值获取定义名称
+	 * @param index 枚举index值
+	 * @return 枚举value值
+	 */
+	public static String getValueByIndex(String index){
+		ProNutritionalNameEnum[] values = ProNutritionalNameEnum.values();
+		for (ProNutritionalNameEnum enu : values){
+			if (index == enu.getIndex()){
+				return enu.getValue();
+			}
+		}
+		return null;
+	}
+}
