13b28107bf01b4abf480b417842e59e9880e0ccc
mark <mark_chu@ssic.cn>
2016-05-25 17:10:01 +0800

    模块结构修改iain

---
+++ education/education-pro-handle/src/main/java/com/ssic/education/pro/handle/pojo/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 文件名：package-info.java
+ *
+ * 版本信息：
+ * 日期：2016年5月25日
+ * Copyright 足下 Corporation 2016 
+ * 版权所有
+ *
+ */
+
+/**
+ * 此类描述的是：
+ * @author: cwftalus@163.com
+ * @version: 2016年5月25日 下午5:07:08 
+ */
+
+package com.ssic.education.pro.handle.pojo;
