204c4b0e3aa5c7e155d1946d6f3ea93482abfa84
mark <mark_chu@ssic.cn>
2016-05-26 23:47:01 +0800

    delete

--- education/education-common/src/main/java/com/ssic/education/common/provider/service/ICorporateService.java
+++
@@ -1,9 +0,0 @@
-package com.ssic.education.common.provider.service;
-
-import com.ssic.education.common.pojo.ProSupplier;
-
-public interface ICorporateService {
-
-	ProSupplier findSupplierById(String sourceId);
-
-}
