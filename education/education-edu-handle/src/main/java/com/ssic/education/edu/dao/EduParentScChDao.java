27970f828f9b87164700c5d50c643e9e329e6e10
mark <mark_chu@ssic.cn>
2016-05-26 17:09:01 +0800

    11

--- education/education-edu-handle/src/main/java/com/ssic/education/edu/dao/EduParentScChDao.java
+++ education/education-edu-handle/src/main/java/com/ssic/education/edu/dao/EduParentScChDao.java
@@ -1,12 +1,18 @@
 package com.ssic.education.edu.dao;
 
+import java.util.List;
+
 import lombok.Getter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.ssic.education.edu.dto.EduParentScChDto;
 import com.ssic.education.edu.mapper.EduParentScChMapper;
 import com.ssic.education.edu.pojo.EduParentScCh;
+import com.ssic.education.edu.pojo.EduParentScChExample;
+import com.ssic.education.edu.pojo.EduParentScChExample.Criteria;
+import com.ssic.education.utils.constants.DataStatus;
 import com.ssic.education.utils.mybatis.MyBatisBaseDao;
 
 @Repository
@@ -15,6 +21,15 @@ public class EduParentScChDao extends MyBatisBaseDao<EduParentScCh> {
 	@Autowired
 	@Getter
 	private EduParentScChMapper mapper;
+
+	public List<EduParentScCh> searchParentScChDtoList(EduParentScChDto eduParentScChDto) {
+		EduParentScChExample example = new EduParentScChExample();
+		Criteria criteria = example.createCriteria();
+
+
+		criteria.andStatEqualTo(DataStatus.ENABLED);
+		return mapper.selectByExample(example);
+	}
 
 	
 }
