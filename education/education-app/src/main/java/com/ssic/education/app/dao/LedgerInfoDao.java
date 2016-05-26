381cfd398f61aadf73588eb2d9ba7d07dddc9319
SeanYoung <gang_yang@ssic.cn>
2016-05-26 16:42:10 +0800

    配送信息列表接口

--- education/education-app/src/main/java/com/ssic/education/app/dao/LedgerInfoDao.java
+++ education/education-app/src/main/java/com/ssic/education/app/dao/LedgerInfoDao.java
@@ -4,12 +4,25 @@ import java.util.List;
 
 import lombok.Getter;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ssic.education.app.dto.LedgerInfoDto;
 import com.ssic.education.app.dto.LedgerListDto;
+import com.ssic.education.app.dto.LedgerMasterListDto;
 import com.ssic.education.app.mapper.LedgerInfoExMapper;
+import com.ssic.education.common.dto.ProLedgerDto;
+import com.ssic.education.common.mapper.ProLedgerExMapper;
+import com.ssic.education.common.mapper.ProLedgerMapper;
+import com.ssic.education.common.mapper.ProLedgerMasterMapper;
+import com.ssic.education.common.pojo.ProLedger;
+import com.ssic.education.common.pojo.ProLedgerExample;
+import com.ssic.education.common.pojo.ProLedgerMaster;
+import com.ssic.education.common.pojo.ProLedgerMasterExample;
+import com.ssic.education.utils.constants.DataStatus;
+import com.ssic.education.utils.model.PageQuery;
+import com.ssic.education.utils.util.BeanUtils;
 
 /**		
  * <p>Title: LedgerInfoDao </p>
@@ -30,12 +43,64 @@ public class LedgerInfoDao {
 	@Autowired
 	private LedgerInfoExMapper mapper;
 
+	@Getter
+	@Autowired
+	private ProLedgerMapper ledgerMapper;
+	@Getter
+	@Autowired
+	private ProLedgerMasterMapper ledgerMasterMapper;
+
 	public LedgerInfoDto findLedgerByBatchNo(String batchNo) {
 		return mapper.findLedgerByBatchNo(batchNo);
 	}
-	public List<LedgerListDto> findLedgerByWaresId(String waresId)
-	{
+
+	public List<LedgerListDto> findLedgerByWaresId(String waresId) {
 		return mapper.findLedgerByWaresId(waresId);
 	}
-	
+
+	/**
+	 * findAllMaster：查询LedgerMaster列表根据ReceiverId
+	 * @param proLedgerDto
+	 * @param page
+	 * @return
+	 * @exception	
+	 * @author SeanYoung
+	 * @date 2016年5月26日 下午2:31:27
+	 */
+	public List<LedgerMasterListDto> findAllMaster(ProLedgerMaster proLedgerMaster, PageQuery page) {
+		ProLedgerMasterExample example = new ProLedgerMasterExample();
+		ProLedgerMasterExample.Criteria criteria = example.createCriteria();
+		if (StringUtils.isNotBlank(proLedgerMaster.getReceiverId())) {
+			criteria.andReceiverIdEqualTo(proLedgerMaster.getReceiverId());
+		}
+		criteria.andStatEqualTo(DataStatus.ENABLED);
+		if (null != page) {
+			example.setOrderByClause("stat desc,create_time desc limit " + page.getStartNum() + "," + page.getPageSize());
+		}
+		List<ProLedgerMaster> proLedgersMaster = ledgerMasterMapper.selectByExample(example);
+		return BeanUtils.createBeanListByTarget(proLedgersMaster, LedgerMasterListDto.class);
+	}
+
+	/**
+	 * 根据masterId 查出采购品拼接字符串
+	 * getWaresString：一句话描述方法功能
+	 * @param masterId
+	 * @return
+	 * @exception	
+	 * @author SeanYoung
+	 * @date 2016年5月26日 下午2:36:48
+	 */
+	public List<ProLedger> findProLedgerList(ProLedger ProLedger, PageQuery page) {
+		ProLedgerExample example = new ProLedgerExample();
+		ProLedgerExample.Criteria criteria = example.createCriteria();
+		if (StringUtils.isNotBlank(ProLedger.getMasterId())) {
+			criteria.andMasterIdEqualTo(ProLedger.getMasterId());
+		}
+		criteria.andStatEqualTo(DataStatus.ENABLED);
+		if (null != page) {
+			example.setOrderByClause("stat desc,create_time desc limit " + page.getStartNum() + "," + page.getPageSize());
+		}
+		List<ProLedger> proLedgers = ledgerMapper.selectByExample(example);
+		return proLedgers;
+	}
 }
--- education/education-app/src/main/java/com/ssic/education/app/service/impl/LedgerInfoServiceImpl.java
+++ education/education-app/src/main/java/com/ssic/education/app/service/impl/LedgerInfoServiceImpl.java
@@ -1,11 +1,20 @@
 package com.ssic.education.app.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssic.education.app.dao.LedgerInfoDao;
+import com.ssic.education.app.dao.SupplierInfoDao;
 import com.ssic.education.app.dto.LedgerInfoDto;
+import com.ssic.education.app.dto.LedgerMasterListDto;
 import com.ssic.education.app.service.ILedgerInfoService;
+import com.ssic.education.common.pojo.ProLedger;
+import com.ssic.education.common.pojo.ProLedgerMaster;
+import com.ssic.education.utils.model.PageQuery;
+import com.ssic.education.utils.model.PageResult;
 
 /**		
  * <p>Title: LedgerInfoServiceImpl </p>
@@ -24,6 +33,8 @@ public class LedgerInfoServiceImpl implements ILedgerInfoService {
 
 	@Autowired
 	private LedgerInfoDao ledgerInfoDao;
+	@Autowired
+	private SupplierInfoDao supplierInfoDao;
 
 	/** 
 	* (non-Javadoc)   
@@ -34,4 +45,46 @@ public class LedgerInfoServiceImpl implements ILedgerInfoService {
 		return ledgerInfoDao.findLedgerByBatchNo(batchNo);
 	}
 
+	//查询配送信息列表
+	public PageResult<LedgerMasterListDto> findMasterList(String receiverId, PageQuery page) {
+		ProLedgerMaster ledgerMaster = new ProLedgerMaster();
+		List<LedgerMasterListDto> LedgerMasterList = ledgerInfoDao.findAllMaster(ledgerMaster, page);
+		List<LedgerMasterListDto> result = new ArrayList<LedgerMasterListDto>();
+		for (LedgerMasterListDto lmld : LedgerMasterList) {
+			ProLedger Ledger = new ProLedger();
+			Ledger.setMasterId(lmld.getId());
+			List<ProLedger> ProLedgerList = ledgerInfoDao.findProLedgerList(Ledger, null);
+			lmld.setStock(listToString(ProLedgerList));
+			lmld.setOutset(supplierInfoDao.getSupplierName(lmld.getSourceId()));
+			result.add(lmld);
+		}
+		return new PageResult<LedgerMasterListDto>(page, result);
+	}
+
+	/**
+	 * List转String
+	 * listToString：一句话描述方法功能
+	 * @param list
+	 * @return
+	 * @exception	
+	 * @author Administrator
+	 * @date 2016年5月26日 下午3:31:15
+	 */
+	public String listToString(List<ProLedger> list) {
+		if (list == null) {
+			return null;
+		}
+		StringBuilder result = new StringBuilder();
+		boolean flag = false;
+		for (ProLedger proLedger : list) {
+			if (flag) {
+				result.append("," == null ? "" : ",");
+			} else {
+				flag = true;
+			}
+			result.append(proLedger.getName());
+		}
+
+		return result.toString();
+	}
 }
