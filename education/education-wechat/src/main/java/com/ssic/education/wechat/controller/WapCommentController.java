5a3e016b790885603b0dd011ce319a9d47534ec9
mark <mark_chu@ssic.cn>
2016-05-27 17:25:32 +0800

    66

--- education/education-wechat/src/main/java/com/ssic/education/wechat/controller/WapCommentController.java
+++ education/education-wechat/src/main/java/com/ssic/education/wechat/controller/WapCommentController.java
@@ -74,6 +74,7 @@ public class WapCommentController extends BaseController {
 		
 		ModelAndView mv = getModelAndView();
 
+		eduParentPackCommentDto.setParentId(parentId);
 		List<EduParentPackCommentDto> dataList = iEduParentPackCommentService.searchComment(eduParentPackCommentDto);
 		
 		mv.addObject("dataList",dataList);
