dd98450cd857f390f925113ee68c5d9b1caf9a64
leo_zhang@ssic.cn <leo_zhang@ssic.cn>
2016-05-29 14:36:42 +0800

    change compile source level

--- education/education-edu-handle/src/main/java/com/ssic/education/handle/dao/EduParentScChDao.java
+++ education/education-edu-handle/src/main/java/com/ssic/education/handle/dao/EduParentScChDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import lombok.Getter;
 
-import org.codehaus.plexus.util.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
