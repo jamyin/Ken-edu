ebf659978ade6ec2afc01f43f50d087901e6eeac
leo_zhang@ssic.cn <leo_zhang@ssic.cn>
2016-05-30 14:53:00 +0800

    commit

--- education/doc/init-data.sql
+++ education/doc/init-data.sql
@@ -1,4 +1,4 @@
-set autocommit=0;
+START TRANSACTION;
 
 #初始化教委
 insert into t_edu_committee(id,name,area_code,type,create_time,last_update_time,stat) values
@@ -22,38 +22,38 @@ insert into t_edu_committee(id,name,area_code,type,create_time,last_update_time,
 
 #初始化教委用户账号
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,0,1,now(),now(),1 from t_edu_committee where name = '上海市教委';
+select UUID(),name,name,md5("123456"),id,0,1,now(),now(),1 from t_edu_committee where name = '上海市教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '黄埔区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '黄埔区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '静安区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '静安区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '徐汇区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '徐汇区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '长宁区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '长宁区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '普陀区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '普陀区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '虹口区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '虹口区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '杨浦区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '杨浦区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '闵行区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '闵行区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '嘉定区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '嘉定区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '宝山区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '宝山区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '浦东新区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '浦东新区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '松江区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '松江区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '金山区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '金山区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '青浦区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '青浦区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '奉贤区教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '奉贤区教委';
 insert into t_edu_users(id,name,user_account,password,source_id,source_type,isAdmin,create_time,last_update_time,stat)
-select UUID(),name,'shs',md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '崇明县教委';
+select UUID(),name,name,md5("123456"),id,2,1,now(),now(),1 from t_edu_committee where name = '崇明县教委';
 
 commit;
