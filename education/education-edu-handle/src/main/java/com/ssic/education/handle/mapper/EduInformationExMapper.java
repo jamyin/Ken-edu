ceaf73c2c9f4adafc11fa46177971179fdff7a4e
menghua_gao <menghua_gao@ssic.cn>
2016-05-30 09:16:03 +0800

    2016530 9.15

---
+++ education/education-edu-handle/src/main/java/com/ssic/education/handle/mapper/EduInformationExMapper.java
@@ -0,0 +1,11 @@
+package com.ssic.education.handle.mapper;
+
+import java.util.List;
+
+import com.ssic.educateion.common.dto.EduInformationDto;
+
+public interface EduInformationExMapper {
+
+//	List<EduInformationDto> list();
+
+}
