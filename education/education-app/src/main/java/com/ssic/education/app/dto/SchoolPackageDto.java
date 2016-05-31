382dca6d568ebb562a80de51f76f42306100809d
jamyin <jamhihi@126.com>
2016-05-31 17:17:35 +0800

    Signed-off-by: jamyin <jamhihi@126.com>

--- education/education-app/src/main/java/com/ssic/education/app/dto/SchoolPackageDto.java
+++ education/education-app/src/main/java/com/ssic/education/app/dto/SchoolPackageDto.java
@@ -3,9 +3,7 @@ package com.ssic.education.app.dto;
 import lombok.Data;
 import lombok.ToString;
 
-import com.ssic.educateion.common.dto.EduCanteenDto;
 import com.ssic.educateion.common.dto.EduSchoolDto;
-import com.ssic.educateion.common.dto.EduSchoolSupplierDto;
 import com.ssic.educateion.common.dto.ProPackagesDto;
 import com.ssic.education.utils.model.PageResult;
 
@@ -21,8 +19,8 @@ import com.ssic.education.utils.model.PageResult;
 public class SchoolPackageDto {
 	
 	private EduSchoolDto eduSchoolDto;
-	private EduCanteenDto eduCanteenDto;
-	private EduSchoolSupplierDto eduSchoolSupplierDto;
+	//private EduCanteenDto eduCanteenDto;
+	//private EduSchoolSupplierDto eduSchoolSupplierDto;
 	private PageResult<ProPackagesDto> proPackagesDto;
     
 }
