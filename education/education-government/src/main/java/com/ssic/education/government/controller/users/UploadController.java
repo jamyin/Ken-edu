8ac971e9b5cf9bc5f269663be9c8c19aafc940e6
pengpeng_zhang <pengpeng_zhang@ssic.cn>
2016-06-06 11:59:35 +0800

    cc

--- education/education-government/src/main/java/com/ssic/education/government/controller/users/UploadController.java
+++ education/education-government/src/main/java/com/ssic/education/government/controller/users/UploadController.java
@@ -122,7 +122,7 @@ public class UploadController {
 			throws IOException {
 		// 如果只是上传一个文件，则只需要MultipartFile类型接收文件即可，而且无需显式指定@RequestParam注解
 		// 如果想上传多个文件，那么这里就要用MultipartFile[]类型来接收文件，并且还要指定@RequestParam注解
-		String context = "";
+		String context = "/upload";
 		String realPath = PropertiesUtils.getProperty("upload.url");
 		String fileDe = DateUtils.format(new Date(), DateUtils.YMD);
 		String fileName = FileUtils.getUploadFileName(myfile.getOriginalFilename());
