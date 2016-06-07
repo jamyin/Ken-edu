9e97c50d7163fbae86f3aceb8d5b9b9fee2fe7ad
jamyin <jamhihi@126.com>
2016-06-07 09:59:28 +0800

    Signed-off-by: jamyin <jamhihi@126.com>

--- education/education-app/src/main/java/com/ssic/education/app/service/impl/InformationServiceImpl.java
+++ education/education-app/src/main/java/com/ssic/education/app/service/impl/InformationServiceImpl.java
@@ -40,12 +40,13 @@ public class InformationServiceImpl implements IInformationService{
 		//String realPath = "127.0.0.1:8091";                    //拼接图片显示路径
 		for (EduInformationDto dto : informationDtoList) {
 			if(StringUtils.isNotEmpty(dto.getPic())){
-				String pics[] = dto.getPic().split(";");
+				dto.setPic(realPath +dto.getPic()) ;
+				String pics[] = dto.getPic().split(";");      //pics用于存放多张图片的路径
 				if(pics.length >0){
 					for(int i =0 ;i <pics.length ;i++){
 						pics[i] = realPath + pics[i];
 					}
-					dto.setPics(pics);                       //pics用于存放多张图片的路径
+					dto.setPics(pics);                       
 				}
 			}
 		}
