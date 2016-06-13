ceb6e8599b3e656b12db69b59f4995f970b74260
mark <mark_chu@ssic.cn>
2016-06-13 10:18:12 +0800

    44

--- education/education-wechat/src/main/java/com/ssic/education/wechat/controller/IndexController.java
+++ education/education-wechat/src/main/java/com/ssic/education/wechat/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.ssic.education.wechat.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,9 @@ public class IndexController extends BaseController{
 	
 	@RequestMapping(value="index")
 	public ModelAndView indexController(EduSchoolDto eduSchoolDto,String code) {
-		
-		setWeixinOpenId(code);
+		if(StringUtils.isNotEmpty(code)){
+			setWeixinOpenId(code);	
+		}
 		
 		ModelAndView mv = getModelAndView();
 		
