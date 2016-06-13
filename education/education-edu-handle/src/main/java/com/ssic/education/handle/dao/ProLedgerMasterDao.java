e81583a5965a7b0130ed2fcceb5e2c165611f5f0
mark <mark_chu@ssic.cn>
2016-06-13 09:33:00 +0800

    12

--- education/education-edu-handle/src/main/java/com/ssic/education/handle/dao/ProLedgerMasterDao.java
+++ education/education-edu-handle/src/main/java/com/ssic/education/handle/dao/ProLedgerMasterDao.java
@@ -31,7 +31,7 @@ public class ProLedgerMasterDao extends MyBatisBaseDao<ProLedgerMaster>{
 		}
 		
 		criteria.andStatEqualTo(DataStatus.ENABLED);
-		
+		example.setOrderByClause(" haul_status asc,create_time desc ");
 		return mapper.selectByExample(example);
 	}
 	
