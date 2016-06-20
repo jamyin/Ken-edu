2129813f7b504d1d9fe57a46b8b888f964b6314c
pengpeng_zhang <pengpeng_zhang@ssic.cn>
2016-06-20 14:36:06 +0800

    cc

--- education/education-government/src/main/webapp/static/js/regUtils.js
+++ education/education-government/src/main/webapp/static/js/regUtils.js
@@ -1,63 +1,32 @@
-$(function() {	
-//	//失去焦点事件
-//	$("#picCaptcha").on("blur",function(){
-//		var picCaptcha = $("#picCaptcha").val();
-//		var dataParam = {picCaptcha:picCaptcha};
-//		$.ajax({
-//			url : '/ajax/checkRandom.htm',
-//			type : "POST",
-//			data : dataParam,
-//			dataType : 'json',
-//			success : function(data) {
-//				if(data.status == 500){
-//					$("#sendMessage").attr("disabled","disabled");
-//					$("#sendMessage").css("background","#c6c6c6");
-//					layer.alert(data.message, {icon: 2});
-//				}else{
-//					$("#sendMessage").css("background","#60ac62");
-//					$("#sendMessage").removeAttr("disabled");
-//				}
-//			}
-//		});
-//	});
-////	$("#picCaptcha").on("blur",function(){
-////		var picCaptcha = $("#picCaptcha").val();
-////		if(picCaptcha=="" || picCaptcha==null){
-////			return false;
-////		}		
-////		var dataParam = {picCaptcha:picCaptcha};
-////		$.ajax({
-////			url : '/ajax/checkRandom.htm',
-////			type : "POST",
-////			data : dataParam,
-////			dataType : 'json',
-////			success : function(data) {
-////				if(data.status == 500){
-////					//$("#sendMessage").attr("disabled","disabled");
-////					//$("#sendMessage").css("background","#c6c6c6");
-////					layer.alert(data.message, {icon: 2});
-////				}else{
-////					//$("#sendMessage").css("background","#60ac62");
-////					//$("#sendMessage").removeAttr("disabled");
-////				}
-////			}
-////		});
-////	});
-	$("#sendMessage").click(function(){
-		var mobilePhone = $("#mobileNo").val();
-		var phone_reg  = /(^13\d{9}$)|(^14)[5,7]\d{8}$|(^15[0,1,2,3,5,6,7,8,9]\d{8}$)|(^17)[6,7,8]\d{8}$|(^18\d{9}$)/g ;  
-		if (!phone_reg.test(mobilePhone)) {
-			layer.msg("手机格式不正确", {icon: 2,time: 800});//alert_redtext('mobile','手机格式不正确')
-			return;
-		}			
+$(function() {		
+	//失去焦点事件
+	$("#picCaptcha").on("blur",function(){
+		var picCaptcha = $("#picCaptcha").val();
+		var dataParam = {picCaptcha:picCaptcha};
+		$.ajax({
+			url : '/ajax/checkRandom.htm',
+			type : "POST",
+			data : dataParam,
+			dataType : 'json',
+			success : function(data) {
+				if(data.status == 500){
+					$("#sendMessage").attr("disabled","disabled");
+					$("#sendMessage").css("background","#c6c6c6");
+					layer.alert(data.message, {icon: 2});
+				}else{
+					$("#sendMessage").css("background","#60ac62");
+					$("#sendMessage").removeAttr("disabled");
+				}
+			}
+		});
+	});
+	$("#sendMessage").on("click",function(){
 		var picCaptcha = $("#picCaptcha").val();
 		if (picCaptcha.length<=0 || picCaptcha == 'undefined') {
-//			layer.alert("请输入图片验证码~", {icon: 2});
-			layer.msg("请输入图片验证码", {icon: 2,time: 800});//alert_redtext('mobile','手机格式不正确')
+			layer.alert("请输入图片验证码~", {icon: 2});
 			return;
 		}		
 		var mobilePhone = $("#mobileNo").val();
-		
 		var dataParam = {picCaptcha:picCaptcha,mobilePhone:mobilePhone};
 		$.ajax({
 			url : '/ajax/SMS/send.htm',
@@ -66,11 +35,9 @@ $(function() {
 			dataType : 'json',
 			success : function(data) {
 				if(data.status == 500){
-					layer.alert(data.message, {icon: 2,time: 800});
+					layer.alert(data.message, {icon: 2});
 				}else{
-					var obj =  $("#sendMessage");
-					$(obj).attr("disabled","disabled");
-					setMessagetime(obj);
+					
 				}
 			}
 		});
@@ -86,13 +53,9 @@ $(function() {
 			dataType : 'json',
 			success : function(data) {
 				if(data.status == 500){
-					$("#messageValid").val("")
 					$("#sursSubmit").attr("disabled","disabled");
 					$("#sursSubmit").css("background","#c6c6c6");
-//					layer.alert(data.message, {icon: 2});
-					//layer.msg(data.message, {icon: 2,time: 800});
-					$("#messageValid").attr("placeholder",data.message);
-//					alert_redtext('messageValid',data.message)
+					layer.alert(data.message, {icon: 2});
 				}else{
 					$("#sursSubmit").css("background","#60ac62");
 					$("#sursSubmit").removeAttr("disabled");
@@ -105,18 +68,3 @@ $(function() {
 		$(this).attr("src","/ajax/drawRandom.htm?"+Math.random());
 	});
 });
-
-var countM = 90;
-function setMessagetime(obj) {
-	if (countM == 0) {
-		$(obj).removeAttr("disabled");
-		$(obj).val("获取短信验证码");
-		return false;
-	} else {
-		$(obj).val("获取短信验证码" + countM + "");
-		countM--;
-	}
-	setTimeout(function() {
-		setMessagetime(obj)
-	}, 1000)
-}
