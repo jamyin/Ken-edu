6789c7e86be26e60a0688da7740fd0a7b3fb1c92
admin <admin@admin-PC>
2016-06-22 09:33:16 +0800

    同步加载完成之后 执行bind事件

--- education/education-provider/src/main/webapp/reg/js/reg.js
+++ education/education-provider/src/main/webapp/reg/js/reg.js
@@ -2,15 +2,25 @@ function laodcommittee(){
 	$.ajax({
 		url:'area/committee?type=2',
 		type:"POST",
+		async: false,
 		dataType:'json',
 		success:function(data){
 			var items = data.data
 			var committeeList = $("#committeeList");
 			committeeList.empty();
+			committeeList.append("<input id='chk_all'  style='margin:5px;' type='checkbox''>全选</input>")
 			$.each(data.data, function(index, item) {
 				committeeList.append("<input name='committees' style='margin:5px;' type='checkbox' value='"+item['id']+"'>"+item['name']+"</input>")
 			});
 			
+			bindClickFunction();
+			
 		}
 	});
 };
+
+function bindClickFunction(){
+	$("#chk_all").click(function(){
+	     $("input[name='committees']").prop("checked",$(this).prop("checked"));
+	});
+}
